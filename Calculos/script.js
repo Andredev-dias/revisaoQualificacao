2c0e820d68520af52e9d2e6521080d8c7b4289d6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-21 15:52:09 -0300

    subindo na nuvem para alunos conferirem os codigos

---
+++ Calculos/script.js
@@ -0,0 +1,41 @@
+function soma() {
+    var n1 = parseFloat(document.getElementById("so1").value)
+    var n2 = parseFloat(document.getElementById("so2").value)
+    var resposta = document.getElementById("respSoma")
+    resposta.textContent = n1 + n2
+}
+
+function sub() {
+    var n1 = parseFloat(document.getElementById("sub1").value)
+    var n2 = parseFloat(document.getElementById("sub2").value)
+    document.getElementById("respSub").textContent = n1 - n2
+}
+
+function multi() {
+    var n1 = parseFloat(document.getElementById("mu1").value)
+    var n2 = parseFloat(document.getElementById("mu2").value)
+    document.getElementById("respMulti").textContent = n1 * n2
+}
+
+function divisao() {
+    var n1 = parseFloat(document.getElementById("div1").value)
+    var n2 = parseFloat(document.getElementById("div2").value)
+    document.getElementById("respDiv").textContent = n1 / n2
+}
+
+function media() {
+    var n1 = parseFloat(document.getElementById("m1").value)
+    var n2 = parseFloat(document.getElementById("m2").value)
+    var n3 = parseFloat(document.getElementById("m3").value)
+    var media = document.getElementById("respMedia")
+    var res = (n1 + n2 + n3) / 3;
+
+    if (res >= 7) {
+        media.textContent = res;
+        media.style.color = "green"
+        media.style.backgroundColor = "lightgreen"
+    } else {
+        media.textContent = res;
+        media.style.color = "red"
+    }
+}
