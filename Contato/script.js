088e09b004d7374ab2b85d27927c2ab708cbf06e
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-25 14:30:21 -0300

    metade da logica de envio de mensagem por zap

---
+++ Contato/script.js
@@ -0,0 +1,34 @@
+var nomeGlobal
+var mensagemGlobal
+var dateGlobal
+
+function formatar(date){
+    var options = {
+        month: "numeric",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    }
+
+    return date.toLocaleString("pt-BR", options)
+}
+function conferirMensagemWhatsApp(){
+    var nome = document.getElementById("nome").value
+    var mensagem = document.getElementById("mensagem").value
+    var date = new Date()
+    
+    nomeGlobal = nome
+    mensagemGlobal = mensagem
+    dateGlobal = date
+    
+    document.getElementById("confNome").textContent = nome
+    document.getElementById("confMsg").textContent = mensagem
+    document.getElementById("confDate").textContent = formatar(date);
+}
+
+
+function enviar(){
+   console.log(nomeGlobal, mensagemGlobal, dateGlobal)
+}
