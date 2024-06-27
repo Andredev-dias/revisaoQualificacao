5cc716951bc4867e7dd66854961e3f5933934e88
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-27 15:53:17 -0300

    logica de idade e verificacao com template string e concatenacao

---
+++ AgeChecker/script.js
@@ -0,0 +1,31 @@
+function checkMajority(){
+    document.getElementById("clientAge").textContent = " "
+    var wasBorn = document.getElementById("clientBirth").valueAsNumber
+    var res = document.getElementById("result")
+
+    if(isNaN(wasBorn)){
+        res.textContent = "Type only numbers"
+        res.style.color = "red"
+        return;
+    }
+
+    const thisYear = new Date().getFullYear();
+    const age = thisYear - wasBorn;
+    
+    if(age >= 18){
+        res.textContent = "Over 18, ENTRY"
+        res.style.color = "green"
+        document.getElementById("clientAge").textContent = `${age} - years old`
+    }else{
+        res.textContent = "Under 18, NOT ALLOWED"
+        res.style.color = "orange"
+        document.getElementById("clientAge").textContent = age + " - " + " years old"
+    }
+}
+
+
+
+
+
+
+
