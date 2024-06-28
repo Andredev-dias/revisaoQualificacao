b784fe10f1c199a742327cc019a7ee14eba91076
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-28 15:38:49 -0300

    logica do change pic

---
+++ ChangePic/script.js
@@ -0,0 +1,38 @@
+function changeImage(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    var imagem = document.querySelector("#profile")
+    var btn = document.getElementById("btnStyle")
+    var dt = document.getElementById("data")
+    var paragraph = document.getElementById("p")
+  
+    var registro = new Date()
+    dt = registro
+
+    if(html.classList.contains("light")){
+        imagem.setAttribute("src", "../Imagens/img1.png")
+        btn.style.color = "lightgreen"
+        btn.style.borderColor = "lightgreen"
+        paragraph.style.color = "lightgreen"
+        document.getElementById("data").textContent = formatarData(dt)
+    }else{
+        imagem.setAttribute("src", "../Imagens/img2.png")
+        btn.style.color = "lightpink"
+        btn.style.borderColor = "lightpink"
+        paragraph.style.color = "lightpink"
+        document.getElementById("data").textContent = formatarData(dt)
+    }
+
+}
+
+function formatarData(item){
+    var options = {
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    };
+    return item.toLocaleString("pt-BR", options)
+}
