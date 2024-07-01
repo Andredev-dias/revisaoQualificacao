231c08db7869b14023e66fca1e9100041308e9b9
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-07-01 15:00:50 -0300

    intervalo

---
+++ TableToExcel/script.js
@@ -0,0 +1,11 @@
+function adicionarItem(){
+    //Obter os valores dos campos de entrada pegando pelo id do input
+    var nome = document.getElementById("nome").value 
+    var valor = document.getElementById("valor").value 
+    var quantidade = document.getElementById("quantidade").value 
+    //debugar e ou conferir se os valores estão vindo realmente
+    console.log(nome, valor, quantidade)
+
+    //Validar se os campos estão preenchidos
+
+}
